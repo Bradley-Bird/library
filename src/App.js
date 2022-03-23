2014647d4de0c52426ee700e84ce870f05550c5e
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-23 11:15:16 -0700

    router added, bookdetail link takes to book page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import BookDetail from './views/Books/BookDetail';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route path="/books/:id">
+            <BookDetail />
+          </Route>
+          <Route path="/books">
+            <BookList />
+          </Route>
+          <Route path="/">
+            <BookList />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
